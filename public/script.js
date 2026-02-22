d0f1ae7c3bc0c8f22f5c0343d01d8392dbc21ecf
Ghost_the_killer24 <88981553+24-ghost-the-killer@users.noreply.github.com>
2026-02-22 22:32:09 +0100

    feat: Updated refreshing and checking interval

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 let telemetry = [];
 let expandedNodes = new Set(['section-operational', 'section-degraded', 'initialized']);
 let query = "";
-const REFRESH_INTERVAL_SEC = 5;
+const REFRESH_INTERVAL_SEC = 30;
 let lastFetchSec = -1;
 let isActivelyFetching = false;
 
